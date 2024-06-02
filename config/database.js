b96c06c9aa32d90c093d84a9b463b91304f36b99
Shanmugapriya26 <spshanmugi648@gmail.com>
2024-06-02 21:32:12 +0530

    db: credentials moved to .env

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('movie_library', 'root', 'Sampriya@260903', {
-    host: 'localhost',
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
